3614c57ee83d3a821658467012bfa95b4ad9565f
liquidcronos <jan-baumgaertner@web.de>
2018-05-29 09:32:40 +0200

    ego: fixed stutter while following lucy

--- Ego.gd
+++ Ego.gd
@@ -17,6 +17,6 @@ func _physics_process(delta):
 		if Input.is_action_pressed("ui_leftclick"):    #moves Ego where mouse is clicked
  			move_and_slide(SPEED*(get_global_mouse_position() - pos_ego))
 	else:
-			move_and_slide(SPEED*(pos_lucy - pos_ego))
+			move_and_slide((pos_lucy - pos_ego))
 		
 	
